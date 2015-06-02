b44f033a13582f42d41aa68426416b8157b32aa7
ksc4130 <ksc4130@gmail.com>
2015-06-01 20:54:44 -0400

    added subscriptions

---
+++ server/models/subscriber.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var subscriberSchema = mongoose.Schema({
+    email: {type: String, unique: true, required: '{PATH} is required'},
+    subscriptionDate: {type: Date, required: '{PATH} is required'}
+});
+
+var Subscriber = mongoose.model('Subscriber', subscriberSchema);
